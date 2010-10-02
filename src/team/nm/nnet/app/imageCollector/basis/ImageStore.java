f209d64ad8d5b7f7b2d6a361191ef2472fb1c94e
Chimung.hutech <Chimung.hutech@gmail.com>
2010-10-02 09:15:55 +0000

    establish project's skeleton and create image store

---
+++ src/team/nm/nnet/app/imageCollector/basis/ImageStore.java
@@ -0,0 +1,128 @@
+package team.nm.nnet.app.imageCollector.basis;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+import team.nm.nnet.util.IOUtils;
+import team.nm.nnet.util.ImageUtils;
+
+public class ImageStore {
+    
+    private final 	double[] 	ANGLES = {10, 20, 30};
+    private final 	int 		WIDTH_SPAN = 30;    
+    private final 	int 		HEIGHT_SPAN = 20;
+    private 		int 		frameWidth = 150;
+    private 		int 		frameHeight = 200;    
+
+    public long createPngFaces(String srcPath, String destPath) {
+    	long count = 0;
+    	List<String> fileNames = IOUtils.listFileName(srcPath);
+    	if((fileNames == null) || (!IOUtils.exists(destPath))) {
+    		return count;
+    	}
+        for(String filename : fileNames) {
+            BufferedImage bufferedImage = ImageUtils.load(srcPath + "/" + filename);
+            if(bufferedImage != null) {
+            	// Hiệu chỉnh khung hình
+            	bufferedImage = ImageUtils.scale(bufferedImage, frameWidth, frameHeight);
+            	File file = new File(destPath + "/" + filename);
+                ImageUtils.drawImageToJpgByteStream(bufferedImage, file);
+                count++;
+                // Tạo ra các biến thể
+                for(double degree : ANGLES) {
+                	// Xoay hình 1 gốc degree
+                	BufferedImage bufImgVariation = ImageUtils.rotateAndScale(bufferedImage, degree);
+                    if(bufImgVariation != null) {
+                        file = new File(destPath + "/" + degree + "_" + filename);
+                        ImageUtils.saveToPng(bufImgVariation, file);
+                        count++;
+                    }
+                    // Lấy đối xứng d�?c ảnh đã xoay
+                    bufImgVariation = ImageUtils.flip(bufImgVariation, false);
+                    if(bufImgVariation != null) {
+                        file = new File(destPath + "/" + degree + "_x_" + filename);
+                        ImageUtils.saveToPng(bufImgVariation, file);
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    
+    public long createJpgFaces(String srcPath, String destPath) {
+    	long count = 0;
+    	List<String> fileNames = IOUtils.listFileName(srcPath);
+    	if((fileNames == null) || (!IOUtils.exists(destPath))) {
+    		return count;
+    	}
+        for(String filename : fileNames) {
+            BufferedImage bufferedImage = ImageUtils.load(srcPath + "/" + filename);
+            if(bufferedImage != null) {
+            	// Hiệu chỉnh khung hình
+            	bufferedImage = ImageUtils.scale(bufferedImage, frameWidth, frameHeight);
+            	File file = new File(destPath + "/" + filename);
+                ImageUtils.drawImageToJpgByteStream(bufferedImage, file);
+                count++;
+                // Tạo ra các biến thể
+                for(double degree : ANGLES) {
+                	// Xoay hình 1 gốc degree
+                	BufferedImage bufImgVariation = ImageUtils.rotate(bufferedImage, degree);
+                    if(bufImgVariation != null) {
+                        file = new File(destPath + "/" + degree + "_" + filename);
+                        ImageUtils.drawImageToJpgByteStream(bufImgVariation, file);
+                        count++;
+                    }
+                    // Lấy đối xứng d�?c ảnh đã xoay
+                    bufImgVariation = ImageUtils.flip(bufImgVariation, false);
+                    if(bufImgVariation != null) {
+                        file = new File(destPath + "/" + degree + "_x_" + filename);
+                        ImageUtils.drawImageToJpgByteStream(bufImgVariation, file);
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    
+    public long createNonFaces(String srcPath, String destPath) {
+    	long count = 0;
+    	List<String> fileNames = IOUtils.listFileName(srcPath);
+    	if((fileNames == null) || (!IOUtils.exists(destPath))) {
+    		return count;
+    	}
+        for(String filename : fileNames) {
+            BufferedImage bufferedImage = ImageUtils.load(srcPath + "/" + filename);
+            int width = bufferedImage.getWidth(null);
+    		int height = bufferedImage.getHeight(null);
+            if(bufferedImage != null) {
+                for(int x = 0, y = 0; (x + frameWidth < width) && (y + frameHeight < height); x += WIDTH_SPAN, y += HEIGHT_SPAN) {
+                	// Xoay hình 1 gốc degree
+                	BufferedImage bufImgVariation = bufferedImage.getSubimage(x, y, frameWidth, frameHeight);
+                	File file = new File(destPath + "/" + x + "x" + y + filename);
+                	ImageUtils.drawImageToJpgByteStream(bufImgVariation, file);
+                    count++;                    
+                }
+            }
+        }
+        return count;
+    }
+
+	public int getFrameWidth() {
+		return frameWidth;
+	}
+
+	public void setFrameWidth(int frameWidth) {
+		this.frameWidth = frameWidth;
+	}
+
+	public int getFrameHeight() {
+		return frameHeight;
+	}
+
+	public void setFrameHeight(int frameHeight) {
+		this.frameHeight = frameHeight;
+	}
+}
---
+++ test/team/nm/nnet/app/imageCollector/test/TestImgageStore.java
@@ -0,0 +1,65 @@
+package team.nm.nnet.app.imageCollector.test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import team.nm.nnet.app.imageCollector.basis.ImageStore;
+import team.nm.nnet.util.ImageUtils;
+
+public class TestImgageStore extends TestBase {
+	
+	private static String classPath;
+	static {
+		classPath = System.getProperty("user.dir");
+		classPath = classPath.replace('\\', '/');
+		classPath += "/ref/imageStore"; 
+	}
+	
+	public static void main(String[] args) {
+		ImageStore imageStore = (ImageStore) app.getBean("imageStore");
+        long count = imageStore.createJpgFaces(classPath + "/faces", classPath + "/faces/output");
+        System.out.println("--- " + count + " faces were created!");
+        count = imageStore.createNonFaces(classPath + "/nonFaces", classPath + "/nonFaces/output");
+        System.out.println("--- " + count + " non-faces were created!");
+        
+//    	testFunction();
+        System.out.println("---- Finished");
+    }
+    
+    static void testFunction() {
+    	String filename = "ref/imageStore/faces/a.jpg";
+        BufferedImage bufferedImage = ImageUtils.load(filename);        
+        
+        show(ImageUtils.grayScale(bufferedImage));
+        
+        BufferedImage changedBuffer = ImageUtils.resize(bufferedImage, 300, 200);
+        ImageUtils.drawImageToJpgByteStream(changedBuffer, new File(classPath + "/faces/a1.jpg"));
+        show(changedBuffer);
+        
+        changedBuffer = ImageUtils.scale(bufferedImage, 150, 100);
+        ImageUtils.drawImageToJpgByteStream(changedBuffer, new File(classPath + "/faces/a2.jpg"));
+        show(changedBuffer);
+        
+        changedBuffer = ImageUtils.rotate(bufferedImage, 30);
+        ImageUtils.drawImageToJpgByteStream(changedBuffer, new File(classPath + "/faces/a3.jpg"));
+        show(changedBuffer);
+        changedBuffer = ImageUtils.rotateAndScale(bufferedImage, 30);
+        ImageUtils.saveToPng(changedBuffer, new File(classPath + "/faces/a33.jpg"));
+        show(changedBuffer);
+        show(ImageUtils.flip(changedBuffer, false));
+        show(ImageUtils.flip(changedBuffer, true));
+    }
+    
+    public static void show(BufferedImage bufferedImage) {
+        int height = bufferedImage.getHeight(null);
+        int width = bufferedImage.getWidth(null);
+        JFrame frame = new JFrame(width + " x " + height);
+        frame.getContentPane().add(new JLabel( new ImageIcon(bufferedImage)));
+        frame.setSize(width + 50, height + 50);
+        frame.setVisible(true);
+    }
+}
